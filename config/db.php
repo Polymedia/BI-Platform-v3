ff14b6b8ace0cee275e6f7e82c52a353dcdc5ca8
Sharm <for.sneg@gmail.com>
2016-01-28 17:06:14 +0300

    Configuration and installed packages

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=flipbox.ru;dbname=nadejdpav_test',
+    'username' => 'nadejdpav_test',
+    'password' => 'QSyJfkXD',
+    'charset' => 'utf8',
+];
---
+++ config/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+$defaultRoute = 'site/index';
+
+/* visible url => route */
+return [
+
+    //'dashboard' => 'dashboard/index',
+
+];
