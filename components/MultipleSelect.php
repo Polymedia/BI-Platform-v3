a8493893343dbc174f72d0375236913a3173e30e
Sharm <for.sneg@gmail.com>
2016-02-04 16:38:39 +0300

    Fix MultipleSelect widget

--- components/MultipleSelect.php
+++ components/MultipleSelect.php
@@ -4,6 +4,7 @@ namespace app\components;
 
 use yii\base\Widget;
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 class MultipleSelect extends Widget {
 
@@ -24,7 +25,8 @@ class MultipleSelect extends Widget {
     {
         $id = $this->getId();
 
-        echo Html::beginForm('','get', ['id' => $id, 'class'=>'invisible', 'data-pjax' => '1']);
+
+        echo Html::beginForm(Url::canonical(),'get', ['id' => $id, 'class'=>'invisible']);
         echo Html::beginTag("select", ['multiple' => 'multiple', 'style' => 'width: 300px;', 'name' => $this->name]);
         echo Html::renderSelectOptions($this->selectedValues, $this->allValues);
         echo Html::endTag("select");
