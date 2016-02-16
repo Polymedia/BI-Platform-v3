102095e62b6289ae58524ede0a537c3407945f8b
occash <artiom.shal@gmail.com>
2016-02-16 17:07:34 +0300

    Fix array source

--- datasource/ArraySource.php
+++ datasource/ArraySource.php
@@ -25,18 +25,22 @@ class ArraySource extends DataSource
             $this->values = [$this->values];
             
         if (array_key_exists('headerRow', $args))
-            $this->header = (bool)$args['headerRow'];
+            $this->headerRow = (bool)$args['headerRow'];
         else if (count($args) > 2)
-            $this->header = (bool)$args[2];
+            $this->headerRow = (bool)$args[2];
     }
     
     public function header()
     {
-        if ($this->headerRow && count($this->values) > 0)
-            return $this->values[0];
+        if ($this->headerRow && count($this->values) > 0) {
+            if (is_array($this->values[0]))
+                return $this->values[0];
+            else
+                return [$this->values[0]];
+        }
         
         // Dummy header
-        $heads = range(1, $this->columns());
+        $heads = range(1, count($this->values[0]));
         $header = [];
         foreach ($heads as $head)
             $header[] = strval($head);
@@ -46,12 +50,37 @@ class ArraySource extends DataSource
     
     public function filter(Array $filters)
     {
+        $header = $this->header();
         
+        $ifilters = [];
+        foreach ($filters as $key => $value) {
+            $index = array_search($key, $header);
+            $ifilters[$index] = $value;
+        }
+        
+        $values = [];
+        foreach ($this->values as $index => $value) {
+            if ($index == 0 && $this->headerRow)
+                $values[] = $value;
+            else {
+                foreach ($ifilters as $key => $filter) {
+                    if (is_array($value)) {
+                        if (in_array($value[$key], $filter))
+                            $values[] = $value;
+                    } else {
+                        if (in_array($value, $filter))
+                            $values[] = $value;
+                    }
+                }
+            }
+        }
+        $this->values = $values;
     }
 
     public function getIterator()
     {
-        return new \ArrayIterator($this->values);
+        $values = $this->headerRow ? array_slice($this->values, 1) : $this->values; 
+        return new \ArrayIterator($values);
     }
     
     public function count()
